d623658d97c522937898d61e0ed0b36065dd5487
shawn <shawndur@buffalo.edu>
2017-07-23 13:50:05 -0400

    added contact page

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
-  
-  get 'static_pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'static_pages/about'
+
+  get  'static_pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
