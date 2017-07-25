a6090acbb9f38ff25b6b137c7c9e56f01f841646
shawn <shawndur@buffalo.edu>
2017-07-24 22:15:10 -0400

    change site nav on login

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
